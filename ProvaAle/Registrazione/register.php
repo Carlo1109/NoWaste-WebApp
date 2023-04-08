4a71e98e355f948217034c705b04c4e503536606
Alessandro1103 <alexdeluca1103@gmail.com>
2023-04-08 23:41:11 +0200

    qualcosa sto facendo

--- ProvaAle/Registrazione/register.php
+++ ProvaAle/Registrazione/register.php
@@ -4,27 +4,40 @@
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Document</title>
+  <title>Registrazione</title>
 </head>
 <body>
   <?php
     $email = $_POST["email"];
-    $password = $_POST["password"];
+    $pswd = $_POST["password"];
     $nomeP = $_POST["nomeP"];
     $cognomeP = $_POST["cognomeP"];
     $idAzienda = $_POST["idAzienda"];
     $nomeAssociazione = $_POST["nomeAssociazione"];
-    if ($_POST["telDonatore"]) {
+    if ($_POST["telDonatore"]!="") {
       $tel = $_POST["telDonatore"];
     }
     else {
       $tel = $_POST["telAssociazione"];
     }
     $dbconn = pg_connect("host=localhost user=postgres password=password port=5433 dbname=Prova");
-    $query = "SELECT * FROM utente where email=$1";
+    $query = "SELECT * FROM utente WHERE email=$1";
     $result = pg_query_params($dbconn,$query,array($email));
     if ($line = pg_fetch_array($result)) {
-      
+      echo "Utente già registrato";
+      //printf("Utente già registrato");
+    }
+    else {
+      $query2 = "INSERT INTO utente (email,pswd,telefono,isassociazione,nomeass,isprivato,nomep,cognomep,isazienda,idaz) VALUES ($1,$2,$3,$4,$5,$6,$7)";
+      $result2 = pg_query_params($dbconn,$query2,array($email,$pswd,$tel,$nomeAssociazione,$nomeP,$cognomeP,$idAzienda));
+      if($result2) {
+        echo "La registrazione è andata a buon fine";
+        //printf("La registrazione è andata a buon fine");
+      }
+      else {
+        die("La registrazione non è andata a buon fine");
+      }
+      pg_close($dbconn);
     }
   ?>
 </body>
