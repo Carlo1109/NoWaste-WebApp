d1063a3d06566781d556c13ac23754d9088ff8bc
Carlo1109 <galilei.c.lasala@gmail.com>
2023-04-14 14:21:25 +0200

    reindirizzamento a donatore.php. Rinomina upload.p

--- myPhp/upload.php
+++ myPhp/upload.php
@@ -1,3 +1,11 @@
+<!DOCTYPE html>
+<html lang="ita">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Upload</title>
+</head>
 <?php
   session_start();
   $dbconn=pg_connect("host=localhost user=postgres password=ltwsql port=5432 dbname=DatabaseProdotti");
@@ -41,8 +49,18 @@
   $result = pg_query($dbconn, $query);
 
   if ($result) {
-      echo "File caricato con successo nel database.";
+    $_SESSION["caricamentoSuccess"] = "Prodotto caricato correttamente";
   } else {
       echo "Errore durante il caricamento del file nel database: " . pg_last_error();
   }
 ?>
+
+<script>
+  <?php
+  if (isset($_SESSION["caricamentoSuccess"])) {
+    echo 'alert("' . $_SESSION["caricamentoSuccess"] . '");';
+    unset($_SESSION["caricamentoSuccess"]);
+    echo 'window.location.href = "../pages/donatore.php";';
+  }
+  ?>
+</script>
