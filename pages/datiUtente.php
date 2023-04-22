d97c77274512061eaae82dcfedbb2abe3104cee5
Carlo1109 <galilei.c.lasala@gmail.com>
2023-04-22 08:48:26 +0200

    sto facendo delle prove per DatuUtente

---
+++ pages/datiUtente.php
@@ -0,0 +1,136 @@
+<?php
+session_start();
+// Connessione al database PostgreSQL
+$dbcon = pg_connect("host=localhost user=postgres password=ltwsql port=5432 dbname=DatabaseUtenti");
+$sessionUser = $_SESSION["username"];
+
+// Esecuzione della query per selezionare i dati dell'utente con email = 'ciao@gmail.com'
+$query = "SELECT * FROM utente WHERE email = '$sessionUser'";
+$result = pg_query($dbcon, $query);
+
+// Recupero i dati dell'utente
+$userData = pg_fetch_assoc($result);
+?>
+<!DOCTYPE html>
+<html lang="it">
+	<head>
+		<meta charset="UTF-8">
+		<meta http-equiv="X-UA-Compatible" content="IE=edge">
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+		<title>NoWaste - Home</title>
+		<link rel="icon" type="image/x-icon" href="../src/iconaLogoN.ico">
+		<link rel="stylesheet" href="../libraries/bootstrap-5.2.3/dist/css/bootstrap.min.css">
+		<link rel="stylesheet" href="../myCss/myCss.css">
+		<link rel="stylesheet" href="../libraries/fontawesome-free-6.4.0-web/css/all.css">
+		<link rel="stylesheet" href="../libraries/sweetalert2.min.css">
+		<script src="../libraries/bootstrap-5.2.3/dist/js/bootstrap.min.js"></script>
+		<script src="../libraries/bootstrap-5.2.3/dist/js/bootstrap.bundle.min.js"></script>
+		<script src="../libraries/jQuery.min.js"></script>
+		<script src="../myJs/myJs.js"></script>
+    <script src="../libraries/sweetalert2.all.min.js"></script>
+	</head>
+	<body>
+		<header>
+		<nav class="navbar navbar-expand-lg bg-gr">
+        <div class="container">
+          <div class="logo">
+            <img src="../src/logo.png" class="imLog" alt="Logo NoWaste">
+          </div>
+          <div class="ricerca">
+						<form class="d-flex" name="search" role="search" onsubmit="searchWeb(event)">
+							<input class="form-control me-2" type="search" placeholder="Cerca sul web..." aria-label="Search" id="searchText" name="searchText">
+							<button class="btn btn-outline-success" type="submit">Search</button>
+						</form>
+					</div>
+        </div>
+      </nav>
+      <nav class="navbar navbar-expand-lg myNav" id="navHome">
+        <div class="container-fluid">
+          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="fa fa-navicon"></span>
+          </button>
+          <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+              <li class="nav-item">
+                <a class="nav-link active selected-text" aria-current="page" href="index.php">Home</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link active" href="soldi.php">Donazioni</a>
+              </li>
+						<?php
+	if (!isset($_SESSION['logged_in']) || $_SESSION['logged_in'] != true) {
+		echo '<li class="nav-item">
+		<a class="nav-link" href="#" onclick="return false;">Area privata</a>
+	</li></ul><button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#myModal">LOGIN</button><div class="modal fade" id="myModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+		<div class="modal-dialog"><div class="modal-content"><div class="modal-header"><h4 class="loginTitle text-black" id="exampleModalLabel">Login</h4><button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button></div><div class="modal-body"><form method="post" action="../myPhp/login.php" name="loginForm" onsubmit="return checkLogin();"><div class="form-floating mb-3"><input type="email" name="username" class="form-control" id="username" placeholder="Inserisci l\'email"><label for="username" class="text-black">Email</label></div><div class="form-floating mb-3"><input type="password"  name="psw" class="form-control" id="psw" placeholder="Password"><label for="psw" class="text-black">Password</label></div></div><div class="modal-footer"><input type="submit" class="btn btn-primary" value="Login"><a href="sigin.php"><button type="button" class="btn btn-secondary">Sign in</button></a></div></form></div></div></div>';
+	}
+	else if ($_SESSION['logged_in'] == true) {
+		echo '<li class="nav-item"><a class="nav-link active" style="cursor: pointer;" onclick="return decidiPagina('.$_SESSION["assBoolean"].');">Area privata</a>
+	</li> </ul><div class="dropdown"><a class="btn dropdown-toggle" role="button" id="userDropdown" data-bs-toggle="dropdown" aria-expanded="false"><i class="fa fa-user"></i></a><ul class="dropdown-menu dropdown-menu-end" aria-labelledby="userDropdown"><li class="Myitem-dropdown"><h6>Email:</h6> '.$_SESSION["username"].' </li><li><a class="dropdown-item" style="cursor: pointer;" href="mieDonazioni.php">Le mie donazioni monetarie</a></li><li><a class="dropdown-item" href="#">Dati utente</a></li><li><hr class="dropdown-divider"></li><li><a class="dropdown-item" onclick="showConfirm();">Elimina profilo</a></li></ul></div>
+
+	
+<button type="button" class="btn btn-danger" onclick="goToLogout();">LOGOUT</button>';
+	}
+?>
+      </nav>
+    </header>
+
+    <h1>Dati dell'utente</h1>
+    <?php 
+      if($_SESSION["assBoolean"]){
+        echo '<p>ASSOCIAZIONE</p>';
+        echo '<p>Nome Associazione: '.$userData['nomeass'].'</p>';
+      }
+      else{
+        echo '<p>DONATORE</p>';
+        if($userData['idaz']==''){
+          echo '<p>nome: '.$userData['nomep'].'</p>
+          <p>cognome: '.$userData['cognomep'].'</p>';
+        }
+        else{
+          echo '<p>Id Azienda: '.$userData['idaz'].'</p>';
+        }
+      }
+    ?>
+    <p>Email: <?php echo $userData['email']; ?></p>
+    <p>Password: <?php echo $userData['pswd']; ?></p>
+    <p>Telefono: <?php echo $userData['telefono']; ?></p>
+    
+    
+    <footer class="bg-dark text-white">
+			<div class="container">
+				<div class="row">
+					<div class="col-md-4">
+						<h4 class="titleFooter">Contatti</h4>
+						<ul class="list-unstyled">
+							<li>Indirizzo: Via Roma 123, 00100 Roma</li>
+							<li>Telefono: 06 1234567</li>
+							<li>Email: nowaste@gmail.com</li>
+						</ul>
+					</div>
+					<div class="col-md-4">
+						<h4 class="titleFooter">Social</h4>
+						<ul class="list-unstyled-icon">
+							<li><a href="https://it-it.facebook.com/" target="_blank"><i class="fab fa-facebook"></i></a></li>
+							<li><a href="https://twitter.com/?lang=it" target="_blank"><i class="fab fa-twitter"></i></a></li>
+							<li><a href="https://www.instagram.com/" target="_blank"><i class="fab fa-instagram"></i></a></li>
+						</ul>
+					</div>
+					<div class="col-md-4">
+						<h4 class="titleFooter">Newsletter</h4>
+						<p>Iscriviti alla nostra newsletter per ricevere tutte le novità</p>
+						<form>
+							<div class="input-group mb-3">
+								<input type="email" class="form-control" placeholder="Email" aria-label="Email" aria-describedby="button-addon2">
+								<input type="submit" class="btn btn-outline-secondary" id="button-addon2" value="Invia"></button>
+							</div>
+						</form>
+					</div>
+				</div>
+			</div>
+		</footer>
+		<button id="pulsante-scroll-up" class="btn btn-primary rounded-circle" onclick="scrollaPaginaSu()">
+			<i class="fas fa-arrow-up"></i>
+		</button>
+	</body>
+</html>
