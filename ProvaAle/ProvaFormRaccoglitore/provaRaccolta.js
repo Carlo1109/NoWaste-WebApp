033e21c7fe506daafb6760794ff0deef5c010c97
Carlo1109 <galilei.c.lasala@gmail.com>
2023-05-17 11:21:57 +0200

    mk

--- ProvaAle/ProvaFormRaccoglitore/provaRaccolta.js
+++
@@ -1,8 +0,0 @@
-function scrollaPaginaDonatore() {
-  const container = document.querySelector('#containerShelf'); 
-  const yOffset = container.getBoundingClientRect().top + window.pageYOffset;
-  window.scrollTo({
-    top: yOffset,
-    behavior: 'smooth'
-  });
-}
--- ProvaAle/Registrazione/prova.js
+++
@@ -1,9 +0,0 @@
-function logout() {
-  $.ajax({
-    url: "logout.php",
-    type: "POST",
-    success: function() {
-      window.location.href="index.php";
-    }
-  })
-}
